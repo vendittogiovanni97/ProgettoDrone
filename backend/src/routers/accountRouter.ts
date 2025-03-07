51d4492eee7d3d6c4be31ce4e3a841114f16db95
vendittogiovanni97 <vendittogiovanni97@hotmail.it>
2025-03-07 12:31:07 +0100

    gestioneRouter

---
+++ backend/src/routers/accountRouter.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+
+const accountRoutes = (app:Router) => {
+  const router = Router();
+
+  router.post("/register",);
+  router.post("/login",);
+  router.post("/logout",);
+  router.put("/password",)
+
+  app.use("/account", router)
+}
+
+export default accountRoutes
