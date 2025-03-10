86643fc3c5e6e84b210b321ff4697a4ed5ce98a1
vendittogiovanni97 <vendittogiovanni97@hotmail.it>
2025-03-10 12:41:15 +0100

    prima migrazione

--- backend/src/db/dbConfig.ts
+++ backend/src/db/dbConfig.ts
@@ -1,24 +1,5 @@
-import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import { PrismaClient } from "@prisma/client";
 
-dotenv.config();
+const dbClient = new PrismaClient();
 
-const mongo_url=process.env.DATABASE_URL;
-
-if (!mongo_url) {
-  console.error("Errore: La variabile DATABASE_URL non è definita nel file .env");
-  process.exit(1);
-}
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(mongo_url);
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (error:any) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-}
-
-
-export default connectDB;
+export default dbClient;
