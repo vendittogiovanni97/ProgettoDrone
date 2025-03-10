f7b9e41ed7b51d2317c44be7924c1833a8abe708
vendittogiovanni97 <vendittogiovanni97@hotmail.it>
2025-03-10 17:24:14 +0100

    aaa

--- backend/src/routers/accountRouter.ts
+++ backend/src/routers/accountRouter.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { register } from "../controllers/auth/register-controller";
-import login from "../controllers/auth/login-controller";
 import { logout } from "../controllers/auth/logout-controllers";
+
 
 const accountRoutes = (app:Router) => {
   const router = Router();
 
-  router.post("/register", register);
-  router.post("/login", login);
+  router.post("/register",);
+  router.post("/login", );
   router.post("/logout",logout);
   router.put("/password",)
 
