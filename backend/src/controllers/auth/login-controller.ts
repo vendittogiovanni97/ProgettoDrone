ec6fc23b85e4243bc9c10b23012a895a6507653c
vendittogiovanni97 <vendittogiovanni97@hotmail.it>
2025-03-10 16:47:57 +0100

    login

---
+++ backend/src/controllers/auth/login-controller.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { SessionManager } from "../../sessionData";
+import { Cookie, SessionData } from "express-session";
+import { LoginInfo } from "../../types/infoSchema";
+import Users from "../../models/userSchema";
+import { AppError } from "../../types/appError";
+import { responseStatus } from "../../constants/statusEnum";
+import { ErrorCodes } from "../../constants/errorCodes";
+import { AppSuccess } from "../../types/succesType";
+
+const login = async (
+  request: Request<undefined, unknown, LoginInfo>,
+  response: Response
+) => {
+  const { body } = request;
+  console.log("dati", body);
+
+  const user = await Users.findOne({
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (user === null) {
+    throw new AppError(
+      responseStatus.BAD_REQUEST,
+      ErrorCodes.INVALID_CREDENTIALS,
+      "Credenziali non valide"
+    );
+  }
+
+  const hashedPassword = user.password;
+  const isCorrect = await bcrypt.compare(body.password, hashedPassword);
+
+  if (!isCorrect) {
+    throw new AppError(
+      responseStatus.BAD_REQUEST,
+      ErrorCodes.INVALID_CREDENTIALS,
+      "Credenziali non valide"
+    );
+  }
+
+  request.session.id!= user.id;
+  request.session.email = user.email!;
+  request.session.username = user.username;
+
+  // Crea la sessione
+  const sessionManager = SessionManager.getInstance();
+  const sessionData: SessionData = {
+    id: user.id,
+    email: user.email!,
+    username: user.username,
+    cookie: new Cookie(),
+  };
+
+  // Salva la sessione
+  sessionManager.createSession(sessionData);
+
+  AppSuccess.getInstance().successResponse(response, "LOGIN_SUCCESS",responseStatus.OK, {id : user.id, username : user.username, email : user.email} )
+
+  return;
+
+};
+
+export default login;
+
+//tag nfg per page personale
