d228dc1637a2a0856bb0306b40a7b70c973b0162
Stefano <stefanoriccio97@hotmail.it>
2025-03-10 21:22:31 +0100

    TabellaDashboardRealTime

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,20 @@ import ForgotPasswordPage from "./pages/ForgettedPassword";
 import DashboardPage from "./pages/Dashboard";
 
 function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login"/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
+                <Route path="/dashboard" element={<DashboardPage/>}/>
 
-        </Routes>
-      </Router>
-  );
+            </Routes>
+        </Router>
+    );
 }
+
 
 export default App;
 
